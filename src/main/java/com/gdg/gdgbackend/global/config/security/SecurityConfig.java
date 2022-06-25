ce2aed57b857cfc84135c1e031f3e034d267e1fb
hyerin-jang <hrjang92@gmail.com>
2022-06-25 18:43:04 +0900

    security config 설정

---
+++ src/main/java/com/gdg/gdgbackend/global/config/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.gdg.gdgbackend.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("memberLoginId")
+                .passwordParameter("password")
+                .failureUrl("/login?isError=true")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+        ;
+
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/register", "/images/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPointImpl())
+        ;
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPointImpl())
+                .accessDeniedPage("/exception/access-denied")
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() throws Exception {
+        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
