489d69b7232d14d6da50691354c129b9c3429e33
hyerin-jang <hrjang92@gmail.com>
2022-06-25 15:48:50 +0900

    Initial commit

---
+++ src/main/java/com/gdg/gdgbackend/GdgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.gdgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdgBackendApplication.class, args);
+	}
+
+}
