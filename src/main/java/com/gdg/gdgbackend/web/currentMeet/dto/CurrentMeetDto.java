ec87317079d71274243bf01a52369c490df0b33f
hyerin-jang <hrjang92@gmail.com>
2022-06-26 05:58:03 +0900

    feat: docker

--- src/main/java/com/gdg/gdgbackend/web/currentMeet/dto/CurrentMeetDto.java
+++ src/main/java/com/gdg/gdgbackend/web/currentMeet/dto/CurrentMeetDto.java
@@ -2,6 +2,7 @@ package com.gdg.gdgbackend.web.currentMeet.dto;
 
 import com.gdg.gdgbackend.domain.board.entity.Board;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,5 +14,6 @@ import java.util.List;
 @ApiModel
 public class CurrentMeetDto {
 
+    @ApiModelProperty
     private List<Board> currentMeetList;
 }
